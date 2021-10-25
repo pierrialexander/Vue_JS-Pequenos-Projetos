d302529f3e0fefa3f1981775a7555f7a3e72504a
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2021-10-25 20:23:33 -0300

    Projeto Pronto
    Projeto Pronto

---
+++ conversor/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="linha">
+      <Conversor moedaA="USD" moedaB="BRL"/>
+      <Conversor moedaA="BRL" moedaB="USD"/>
+    </div>
+    <div class="linha">
+      <Conversor moedaA="CAD" moedaB="BRL"/>
+      <Conversor moedaA="BRL" moedaB="CAD"/>
+    </div>
+     <div class="linha">
+      <Conversor moedaA="EUR" moedaB="BRL"/>
+      <Conversor moedaA="BRL" moedaB="EUR"/>
+    </div>
+    
+       
+  </div>
+</template>
+
+<script>
+import Conversor from './components/Conversor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Conversor
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: auto;
+  max-width: 800px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.linha{
+  display: flex;
+  justify-content: space-between;
+  margin: 40px;
+}
+</style>
